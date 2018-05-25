fafa01646c096d7d4d059be6a48eb7ef3906e868
RubenK01 <rbn994@gmail.com>
2018-05-25 13:59:57 +0200

    cambios cargar img

--- target/classes/static/js/PrincipalCtrl.js
+++ target/classes/static/js/PrincipalCtrl.js
@@ -5,63 +5,80 @@ var myApp = angular.module('DriverApp',['ngMask', 'ui.bootstrap']);
 
 
 //myApp.controller('PrincipalCtrl',['$scope','$http','mantenimientoSrv',function($scope,$http,mantenimientoSrv){
-myApp.controller('PrincipalCtrl',['$scope', '$uibModal',function($scope, $uibModal){
-	$scope.showSignUp = true;
-	$scope.showLogin = false;
+myApp.controller('PrincipalCtrl',['$scope', function($scope){
 	
+	
+//	var $ctrl = this;
+//	$ctrl.items = ['item1', 'item2', 'item3'];
+//	
+	var reader = new FileReader();
+	$scope.listCars = [];
+	$scope.car = {};
+	$scope.addCar = function() {
+		
+		$scope.car.model = $scope.modelCar;
+		$scope.car.matricula = $scope.matriculaCar;
+		$scope.car.color = $scope.colorCar;
+		$scope.car.img = $scope.fileToUpload;
+		
+		$scope.imgCar = $scope.car.img;
+		getImg($scope.fileToUpload);
+		$scope.listCars.push($scope.car);
+	}
+	
+	function getImg(file){
+		 reader.onloadend = function () {
+			 	$scope.imgUrl = reader.result;
+			  }
 
-	$scope.doSignUp = function(){
-	    $scope.showSignUp = true;
-	    $scope.showLogin = false;
+			  if (file) {
+			    reader.readAsDataURL(file);
+			  } else {
+				  $scope.imgUrl = "";
+			  }
 	}
-	
-	$scope.modalAddCar = function() {
-		var modalInstance = $uibModal.open({
-            //backdrop: 'static',
-            templateUrl: '/html/addCar.htm',
-            //controller: 'ventanaCierreCtrl',
-            windowClass: 'modal',
-            resolve: {
-                
-            },
-            size: 'lg'
-        });
-        //TODO - Esto va en un evento de vuelta al origen de apertura del cierre
-        modalInstance.result
-        .then(function () {  //result
-//            if(datosEntrada.origen === 'CAPTURA'){
-//                $rootScope.$broadcast('aceptarCierreCaptura');
-//            } else if (datosEntrada.origen === 'GESTSOL'){
-//                $rootScope.$broadcast('aceptarCierreGestSol');
-//            } else if (datosEntrada.origen === 'GESTSUP'){
-//                $rootScope.$broadcast('aceptarCierreGestSup');
+//	
+//	$scope.modalAddCar = function() {
+//		var modalInstance = $uibModal.open({
+//			ariaLabelledBy: 'modal-title-top',
+//		    ariaDescribedBy: 'modal-body-top',
+//            templateUrl: '/html/addCar.html',
+//            controller: 'ModalInstanceCtrl',
+//            controllerAs: '$ctrl',
+//            size: 'sm',
+//            resolve: {
+//            	items: function () {
+//                    return $ctrl.items;
+//                  }
 //            }
-        }, function (/*data*/) {  //reject
-//            if(datosEntrada.origen === 'CAPTURA'){
-//                $rootScope.$broadcast('cancelarCierreCaptura', data);
-//            } else if (datosEntrada.origen === 'GESTSOL'){
-//                $rootScope.$broadcast('cancelarCierreGestSol', data);
-//            } else if (datosEntrada.origen === 'GESTSUP'){
-//                $rootScope.$broadcast('cancelarCierreGestSup', data);
-//            }
-        });
-	}
-	
-//	$scope.rellenaFecha = function(){
-//		if($scope.fLicense){
-//			if($scope.fLicense.length === 1 && $scope.fLicense !== '1'){
-//				$scope.fLicense = $scope.fLicense + '/';
-//			}
-//		}
-//		else{
-//			$scope.fLicense = '';
-//		}
-//		
-//		
+//            
+//        });
+//        //TODO - Esto va en un evento de vuelta al origen de apertura del cierre
+//        modalInstance.result
+//        .then(function () {  //result
+//        }, function (/*data*/) {  //reject
+//        });
 //	}
 	
 	
+	
 }]);
+
+//myApp.controller('ModalInstanceCtrl', function ($uibModalInstance, items) {
+//	  var $ctrl = this;
+//	  $ctrl.items = items;
+//	  $ctrl.selected = {
+//	    item: $ctrl.items[0]
+//	  };
+//
+//	  $ctrl.ok = function () {
+//	    $uibModalInstance.close($ctrl.selected.item);
+//	  };
+//
+//	  $ctrl.cancel = function () {
+//	    $uibModalInstance.dismiss('cancel');
+//	  };
+//	});
 
 
 //myApp.config(function($routeProvider) {
@@ -77,8 +94,42 @@ myApp.controller('PrincipalCtrl',['$scope', '$uibModal',function($scope, $uibMod
 //    })
 //});
 
+myApp.directive("fileModel",function() {
+	return {
+		restrict: 'EA',
+		scope: {
+			setFileData: "&"
+		},
+		link: function(scope, ele, attrs) {
+			ele.on('change', function() {
+				scope.$apply(function() {
+					var val = ele[0].files[0];
+					scope.setFileData({ value: val });
+				});
+			});
+		}
+	}
+})
 
-//directiva necesaria para hacer funcionar el input type="file", si se usa ng-model en lugar de esta directiva el fichero no se seta en el scope
+myApp.directive("fileread", [function () {
+    return {
+        scope: {
+            fileread: "="
+        },
+        link: function (scope, element, attributes) {
+            element.bind("change", function (changeEvent) {
+                scope.$apply(function () {
+                    scope.fileread = changeEvent.target.files[0];
+                    // or all selected files:
+                    // scope.fileread = changeEvent.target.files;
+                });
+            });
+        }
+    }
+}]);
+
+
+////directiva necesaria para hacer funcionar el input type="file", si se usa ng-model en lugar de esta directiva el fichero no se seta en el scope
 //myApp.directive('fileModel', ['$parse', function ($parse) {
 //    return {
 //        restrict: 'A',
@@ -93,7 +144,7 @@ myApp.controller('PrincipalCtrl',['$scope', '$uibModal',function($scope, $uibMod
 //        }
 //    };
 //}
-
+//
 //]);
 
 
